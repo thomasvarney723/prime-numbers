fb36064a4203bfc6865efbf544719d04b2108311
Thomas Varney <thomasvarney723@gmail.com>
2015-11-07 23:25:08 -0600

    first working version

--- eratosthenes.js
+++ eratosthenes.js
@@ -1,5 +1,3 @@
-// Sieve of Eratosthenes
-
 function primeMap(below) {
     var amap = {};
     for(var i = 2; i < below; i++)
@@ -21,14 +19,49 @@ function existsIn(number, map) {
         return true;
 }
 
-function bangOnMap(number, map) {
+function lessThanSqrt(number, map) {
+    var length = objKeys2Array(map).length;
+    var last = length + 1;
+    if (number < Math.sqrt(last))
+        return true;
+    else
+        return false;
+}
+
+function mutateMapWithComposites(number, map) {
     var incriment = number;
-    while (map[number] < existsIn(number, map)) {
+    while (existsIn(number, map)) {
         map[number] = "composite";
         number = number + incriment;
     }
 }
 
+function nextPrime(currentPrime, map) {
+    var keys = objKeys2Array(map);
+    while (map[currentPrime] === "composite")
+        currentPrime++;
+    return currentPrime;
+}
+
+function pushRemainingPrimes(map, array) {
+    var i = 2;
+    while(existsIn(i, map)) {
+        if(map[i] === null)
+            array.push(i);
+        i++;
+    }
+    return array;
+}
+
 function primes(below) {
-    
+    var primeSet = [];
+    var map = primeMap(below);
+    var currentPrime = 2;
+    while (lessThanSqrt(currentPrime, map)) {
+        primeSet.push(currentPrime);
+        mutateMapWithComposites(currentPrime, map);
+        currentPrime = nextPrime(currentPrime, map);
+    }
+    pushRemainingPrimes(map, primeSet);
+    return primeSet;
 }
