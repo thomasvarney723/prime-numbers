129a665439d6fe3a30670fee04d5012da97531bb
Thomas Varney <thomasvarney723@gmail.com>
2015-11-13 22:47:37 -0600

    inserted comments; corrected isNull(); made nextPrime shorter

--- eratosthenes2.js
+++ eratosthenes2.js
@@ -1,3 +1,8 @@
+/* 
+   The Sieve of Eratosthenes using a javascript array as
+   an associative data structure.
+*/
+
 function range(start, limit, step) {
     var rangeArray = [];
     while(start < limit) {
@@ -7,14 +12,14 @@ function range(start, limit, step) {
     return rangeArray;
 }
 
-function isNull(value) {
-    if (value === null)
-        return false;
-    else
+function isNotNull(value) { // predicate for filtering
+    if (value !== null)
         return true;
+    else
+        return false;
 }
 
-function nullMultiples(multiple, array) {
+function nullMultiples(multiple, array) { // mutates array
     var step = multiple;
     while(multiple < array.length) {
         array[multiple] = null;
@@ -24,22 +29,22 @@ function nullMultiples(multiple, array) {
 
 function nextPrime(currentPrime, array) {
     var successor = currentPrime++;
-    while (true) {
-        if (array[successor] === null)
-            successor++;
-        else
-            return successor;
+    while (array[successor] === null) {
+        successor++;
     }
+    return successor;
 }
 
 function primes(below) {
    var initSet = range(0, below, 1);
-   var primeSet = [];
-   var currentPrime = initSet[2];
+   var primeSet = []; // set of numbers used as a multiple
+   var currentPrime = initSet[2]; // the first multiple is 2
    while (currentPrime < Math.sqrt(initSet.length)) {
        primeSet.push(currentPrime);
-       nullMultiples(currentPrime, initSet);
-       currentPrime = nextPrime(currentPrime, initSet);
+       nullMultiples(currentPrime, initSet); // mutate initSet
+       currentPrime = nextPrime(currentPrime, initSet); 
    }
-   return primeSet.concat(initSet.slice(2).filter(isNull));
+   // return the numbers that were used as multiples and all
+   // inidicies that still contain a number, except 0 and 1
+   return primeSet.concat(initSet.slice(2).filter(isNotNull));
 }
