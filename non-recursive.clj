e079f55f69abd0a042e0c19a2a905bf7d0d4bb25
Thomas Varney <thomasvarney723@gmail.com>
2016-08-26 21:49:11 -0400

    changed function to use transducer

--- non-recursive.clj
+++ non-recursive.clj
@@ -1,4 +1,4 @@
 (defn primes [below]
-  (remove (set (mapcat #(range (* % %) below %)
-                       (range 3 (Math/sqrt below) 2)))
+  (remove (into #{} (mapcat #(range (* % %) below %))
+                (range 3 (Math/sqrt below) 2))
           (cons 2 (range 3 below 2))))
