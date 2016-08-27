5f82a5676d9a1413f8e575beacfc6673034043f4
Thomas Varney <thomasvarney723@gmail.com>
2016-08-26 21:54:04 -0400

    first commit

---
+++ non-recursive.js
@@ -0,0 +1,27 @@
+function primesLessThan(n) {
+    var composites = range(3, Math.sqrt(n), 2)
+	.map(function(x) {
+	    return range(x * x, n, x);
+	})
+	.reduce(concat);
+    var data = [2].concat(range(3, n, 2));
+    return data.filter(doesNotContain(composites));
+}
+
+function range(start, end, step) {
+    var result = [];
+    for(var i = start; i < end; i += step) {
+	result.push(i);
+    }
+    return result;
+}
+
+function concat(a, b) {
+    return a.concat(b);
+}
+
+function doesNotContain(arr) {
+    return function(elem) {
+	return !(arr.indexOf(elem) > -1);
+    };
+}
