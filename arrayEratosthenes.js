8790522fc024917906ebd97d65433c4608a208e2
Thomas Varney <thomasvarney723@gmail.com>
2016-10-21 21:15:10 -0500

    changed array initialization and collection of primes

--- arrayEratosthenes.js
+++ arrayEratosthenes.js
@@ -1,22 +1,26 @@
+
 function primesLessThan(n) {
     if(typeof n !== 'number' || n < 0)
 	throw new Error('Argument must be a non-negative integer.');
     
-    var intArr = Array(n).fill(undefined);     
-    intArr.forEach(function(elem, idx, arr) {  // initialize array to have
-	arr[idx] = idx;                        // indicies as elements
-    });
+    var intArr = Array(n);
     
-    intArr[0] = intArr[1] = false; // 0 and 1 are not primes
+    intArr[0] = intArr[1] = false;  // 0 and 1 are not primes
     
     var current = 2;
     while(current < Math.sqrt(n)) {
 	for(var i = current * current; i < n; i += current) 
-	        intArr[i] = false;  // eliminate multiples of current
+	            intArr[i] = false;  // eliminate multiples of current
 	current++;
-	while(intArr[current] === false) // get the next prime
-	        current++;
+	while(intArr[current] === false)  // get the next prime
+	    current++;
 	}
     
-    return intArr.filter(Number.isInteger);  // collect the primes
+    var primes = [2];  // collect the primes
+    for(var j = 3; j < intArr.length; j += 2) {
+	if(intArr[j] === undefined)
+	    primes.push(j);
+	}
+    
+    return primes;
 }
