6e4e3e9d1fd8da7bdd849abd5c5eb8f10b85efc1
Thomas Varney <thomasvarney723@gmail.com>
2016-11-24 15:07:48 -0600

    working version

---
+++ non-recursive-immutablejs.js
@@ -0,0 +1,12 @@
+//include Immutable CDN
+var i = Immutable;
+
+function primesLessThan(n) {
+    var oddsAnd2 = i.OrderedSet([2]).concat(i.Range(3, n, 2));
+    function multiplesOf(o) {
+	return i.Range(o * o, n, o);
+    }
+    var oddsToSqrt = i.Range(3, Math.sqrt(n), 2);
+    var composites = i.Set(oddsToSqrt.flatMap(multiplesOf));
+    return oddsAnd2.subtract(composites);
+}
