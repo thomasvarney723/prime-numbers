83356019802cc25964b364684687766c1875295b
Thomas Varney <thomasvarney723@gmail.com>
2016-11-25 13:01:23 -0600

    added 'n<=2' check and fixed tabbing

--- non-recursive-mori.js
+++ non-recursive-mori.js
@@ -2,12 +2,15 @@
 var m = mori;
 
 function primesLessThan(n) {
-  var oddsAnd2 = m.cons(2, m.range(3, n, 2)); 
-  function multiplesOf(o) {
-      return m.range(o * o, n, o)
-  }
-  var oddsToSqrt = m.range(3, Math.sqrt(n), 2);
-  var composites = m.into(m.set([]), m.mapcat(multiplesOf), oddsToSqrt); 
-                                     //uses a transducer!
-  return m.remove(composites, oddsAnd2);
+    if(n <= 2)
+	return [];
+
+    var oddsAnd2 = m.cons(2, m.range(3, n, 2)); 
+    function multiplesOf(o) {
+	return m.range(o * o, n, o)
+    }
+    var oddsToSqrt = m.range(3, Math.sqrt(n), 2);
+    var composites = m.into(m.set([]), m.mapcat(multiplesOf), oddsToSqrt); 
+                                       //uses a transducer!
+    return m.remove(composites, oddsAnd2);
 }
